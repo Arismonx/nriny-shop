ff5aabae8d4c2eaf6fae59b44211d7b5381663a7
Arismonx <arismonx@gmail.com>
2025-10-10 01:11:41 +0700

    feat: implement gRPC authentication service with CredentialSearch and RoleCount methods

--- modules/auth/authHandler/authGrpcHandler.go
+++ modules/auth/authHandler/authGrpcHandler.go
@@ -1,11 +1,15 @@
 package authHandler
 
 import (
+	"context"
+
+	authPb "github.com/Arismonx/nriny-shop/modules/auth/authPb"
 	"github.com/Arismonx/nriny-shop/modules/auth/authUsecase"
 )
 
 type (
 	authGrpcHandler struct {
+		authPb.UnimplementedAuthGrpcServiceServer
 		authUsecase authUsecase.AuthUsecaseService
 	}
 )
@@ -13,5 +17,15 @@ type (
 func NewAuthGrpcHandler(
 	authUsecase authUsecase.AuthUsecaseService,
 ) *authGrpcHandler {
-	return &authGrpcHandler{authUsecase}
+	return &authGrpcHandler{
+		authUsecase: authUsecase,
+	}
+}
+
+func (g *authGrpcHandler) CredentialSearch(ctx context.Context, req *authPb.CredentialSearchReq) (*authPb.CredentialSearchRes, error) {
+	return nil, nil
+}
+
+func (g *authGrpcHandler) RoleCount(ctx context.Context, req *authPb.RoleCountReq) (*authPb.RoleCountRes, error) {
+	return nil, nil
 }
