af9592d1636e5f71c58317563acae31f0d965ba7
Arismonx <arismonx@gmail.com>
2025-10-10 02:41:48 +0700

    feat: add Inventory gRPC service with IsAvailableToSell method and corresponding proto definitions

--- command.md
+++ command.md
@@ -14,10 +14,18 @@
     ./modules/player/playerPb/playerPb.proto
 ```
 
-### command proto fiel `Player` service
+### command proto fiel `item` service
 
 ```
     protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     ./modules/item/itemPb/itemPb.proto
+```
+
+### command proto fiel `inventory` service
+
+```
+    protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    ./modules/inventory/inventoryPb/inventoryPb.proto
 ```
--- modules/inventory/inventoryHandler/inventoryGrpcHandler.go
+++ modules/inventory/inventoryHandler/inventoryGrpcHandler.go
@@ -1,11 +1,15 @@
 package inventoryHandler
 
 import (
+	"context"
+
+	inventoryPb "github.com/Arismonx/nriny-shop/modules/inventory/inventoryPb"
 	"github.com/Arismonx/nriny-shop/modules/inventory/inventoryUsecase"
 )
 
 type (
 	inventoryGrpcHandler struct {
+		inventoryPb.UnimplementedInventoryGrpcServiceServer
 		inventoryUsecase inventoryUsecase.InventoryUsecaseService
 	}
 )
@@ -13,5 +17,11 @@ type (
 func NewInventoryGrpcHandler(
 	inventoryUsecase inventoryUsecase.InventoryUsecaseService,
 ) *inventoryGrpcHandler {
-	return &inventoryGrpcHandler{inventoryUsecase}
+	return &inventoryGrpcHandler{
+		inventoryUsecase: inventoryUsecase,
+	}
+}
+
+func (g *inventoryGrpcHandler) IsAvailableToSell(ctx context.Context, req *inventoryPb.IsAvaibleToSellReq) (*inventoryPb.IsAvaibleToSellRes, error) {
+	return nil, nil
 }
