2f2e682e66577019f68291542e3634a75746c866
Arismonx <arismonx@gmail.com>
2025-10-10 02:16:49 +0700

    feat: add Item gRPC service with FindItemInIds method and corresponding proto definitions

--- command.md
+++ command.md
@@ -13,3 +13,11 @@
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     ./modules/player/playerPb/playerPb.proto
 ```
+
+### command proto fiel `Player` service
+
+```
+    protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    ./modules/item/itemPb/itemPb.proto
+```
--- modules/item/itemHandler/itemGrpcHandler.go
+++ modules/item/itemHandler/itemGrpcHandler.go
@@ -1,11 +1,15 @@
 package itemHandler
 
 import (
+	"context"
+
+	itemPb "github.com/Arismonx/nriny-shop/modules/item/itemPb"
 	"github.com/Arismonx/nriny-shop/modules/item/itemUsecase"
 )
 
 type (
 	itemGrpcHandler struct {
+		itemPb.UnimplementedItemGrpcServiceServer
 		itemUsecase itemUsecase.ItemUsecaseService
 	}
 )
@@ -13,5 +17,11 @@ type (
 func NewItemGrpcHandler(
 	itemUsecase itemUsecase.ItemUsecaseService,
 ) *itemGrpcHandler {
-	return &itemGrpcHandler{itemUsecase}
+	return &itemGrpcHandler{
+		itemUsecase: itemUsecase,
+	}
+}
+
+func (g *itemGrpcHandler) FindItemInIds(ctx context.Context, req *itemPb.FindItemInIdsReq) (*itemPb.FindItemInIdsRes, error) {
+	return nil, nil
 }
