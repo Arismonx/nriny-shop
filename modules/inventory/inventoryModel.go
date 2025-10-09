9fe6a61d811f872f76382a2f2ae10dc70196f394
Arismonx <arismonx@gmail.com>
2025-10-09 13:05:29 +0700

    feat: define inventory entity and model structures for inventory management

--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/Arismonx/nriny-shop/modules/item"
+	"github.com/Arismonx/nriny-shop/modules/model"
+)
+
+type (
+	InventoryUpdateReq struct {
+		PlayerId string `json:"player_id" validate:"required,max=64"`
+		ItemId   string `json:"item_id" validate:"required,max=64"`
+	}
+
+	ItemInInventory struct {
+		InventoryId string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlyerInventory struct {
+		PlayerId string `json:"player_id"`
+		*model.PaginateRes
+	}
+)
